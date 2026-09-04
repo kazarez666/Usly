ce8c6e4ffa60dbc1daae2641e314250314906539
kazarez666 <artem007700123@mail.ru>
2026-09-04 23:49:24 +0300

    Load rebuilt Nocturne theme last

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import AppBootstrap from './AppBootstrap'
 import './styles.css'
 import './app-like.css'
 import './mobile-polish.css'
+import './nocturne-redesign.css'
 import './uiHotfix'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><AppBootstrap /></StrictMode>)
