ef8f9e59f99c6c9812f8f29520de0576fd0e87ef
kazarez666 <artem007700123@mail.ru>
2026-09-04 23:23:02 +0300

    Load final mobile polish overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import AppBootstrap from './AppBootstrap'
 import './styles.css'
 import './app-like.css'
+import './mobile-polish.css'
 import './uiHotfix'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><AppBootstrap /></StrictMode>)
