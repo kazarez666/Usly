7577941f0b0ab6c562266794eb781fbd96a8f368
kazarez666 <artem007700123@mail.ru>
2026-09-05 02:27:02 +0300

    Load robust Rush motion layer

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,8 @@ import './nocturne-header-fix.css'
 import './theme-cohesion.css'
 import './rush-effects.css'
 import './rush-effects'
+import './rush-motion-v2.css'
+import './rush-motion-v2'
 import './uiHotfix'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><AppBootstrap /></StrictMode>)
