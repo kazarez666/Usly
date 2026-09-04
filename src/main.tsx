e36dbbac13791c051502d327ce8a4a20d75a91b4
kazarez666 <artem007700123@mail.ru>
2026-09-05 02:36:09 +0300

    Use one Rush engine and mobile performance layer

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,9 @@ import './mobile-polish.css'
 import './nocturne-redesign.css'
 import './nocturne-header-fix.css'
 import './theme-cohesion.css'
-import './rush-effects.css'
-import './rush-effects'
 import './rush-motion-v2.css'
 import './rush-motion-v2'
+import './mobile-performance.css'
 import './uiHotfix'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><AppBootstrap /></StrictMode>)
