c4ba2f929b61c717fecb95a447323687d903d699
kazarez666 <artem007700123@mail.ru>
2026-09-05 05:55:49 +0300

    Load focused full-height Chat layout

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './theme-cohesion.css'
 import './rush-motion-v2.css'
 import './rush-motion-v2'
 import './mobile-performance.css'
+import './chat-focus.css'
 import './uiHotfix'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><AppBootstrap /></StrictMode>)
